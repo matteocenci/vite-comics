0883605ef54931c453db5bfea64b7adffc08b270
matteocenci <matteocenci01@gmail.com>
2024-05-02 15:59:15 +0200

    feat: AppMain and AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <h3>Footer</h3>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
