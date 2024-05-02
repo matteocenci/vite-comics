b00c89efa8a31c52ec4b65a27c73a075981c4ccb
matteocenci <matteocenci01@gmail.com>
2024-05-02 15:55:23 +0200

    feat: setting project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+
+
+<template>
+    <h2>Header</h2>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
