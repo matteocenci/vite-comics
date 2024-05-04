41e5dc64f10c1a4ed89ead67eda734a1fd3012c0
matteocenci <matteocenci01@gmail.com>
2024-05-04 18:30:26 +0200

    feat: adding container comics and card

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,24 +4,7 @@
 export default {
     data() {
         return {
-            footer: [
-                {
-                    title: "DC COMICS",
-                    list: list1
-                },
-                {
-                    title: "SHOP",
-                    list: list2
-                },
-                {
-                    title: "DC",
-                    list: list3
-                },
-                {
-                    title: "SITES",
-                    list: list4
-                }
-            ]
+            
         }
     },
     
